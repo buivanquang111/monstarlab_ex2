abf62e3af1054bd95fe6b448f02c847619f4aa48
buivanquang111 <58690765+buivanquang111@users.noreply.github.com>
2021-04-19 12:04:05 +0700

    update
    update

--- app/src/main/java/com/example/excercise2_monstarlab/MainActivity.kt
+++ app/src/main/java/com/example/excercise2_monstarlab/MainActivity.kt
@@ -1,25 +1,27 @@
 package com.example.excercise2_monstarlab
 
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.os.Handler
 import android.os.Message
+import android.view.GestureDetector
 import android.view.MotionEvent
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
     val messageCount: Int = 1
-    var number: Int = 0
+    var number: Int? = 0
     var z: Int = 0
     var mHandler: Handler = Handler()
     lateinit var thread: Thread
-    var a: Int = 0
-
-
-
-
+    var increase: CountDownTimer? = null
+    var decrease: CountDownTimer? = null
+    var isMoving: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,23 +29,71 @@ class MainActivity : AppCompatActivity() {
 
         printDisply()
 
-        btnTru.setOnClickListener {
-            number--
-            txtSo.text = number.toString()
+        increase = object : CountDownTimer(Long.MAX_VALUE,100){
+            override fun onFinish() {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                number = number!! + 1
+                txtSo.text = number.toString()
+                if (number == 0){
+                    increase?.cancel()
+
+                }
+            }
+
+        }
+        decrease = object : CountDownTimer(Long.MAX_VALUE,100){
+            override fun onFinish() {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                number = number!! -1
+                txtSo.text = number.toString()
+                if (number == 0){
+                    decrease?.cancel()
+
+                }
+            }
 
         }
 
-        btnCong.setOnClickListener {
-            number++
-            txtSo.text = number.toString()
+        btnCong.setOnTouchListener {
+            v, event ->
+            if (event.action == MotionEvent.ACTION_DOWN){
+                btnCong.setBackgroundColor(resources.getColor(R.color.green))
+                increase?.start()
+                decrease?.cancel()
+            }
+            if (event.action == MotionEvent.ACTION_UP){
+                btnCong.setBackgroundColor(resources.getColor(R.color.teal_200))
+                increase?.cancel()
+                delay({decrease?.start()},1000)
+            }
 
+            false
+        }
+        btnTru.setOnTouchListener {
+            v, event ->
+            if (event.action == MotionEvent.ACTION_DOWN){
+                btnTru.setBackgroundColor(resources.getColor(R.color.green))
+                decrease?.start()
+                increase?.cancel()
+            }
+            if (event.action == MotionEvent.ACTION_UP){
+                btnTru.setBackgroundColor(resources.getColor(R.color.teal_200))
+                decrease?.cancel()
+                delay({increase?.start()},1000)
+            }
+            false
         }
 
         rvActivity.setOnTouchListener {
             v, event ->  handleTouch(event)
             true
         }
-
 
 
     }
@@ -55,11 +105,11 @@ class MainActivity : AppCompatActivity() {
         when(action){
             MotionEvent.ACTION_DOWN ->setDown(m)
             MotionEvent.ACTION_MOVE ->setMove(m)
-            MotionEvent.ACTION_UP -> setActionUp()
+            MotionEvent.ACTION_UP -> delay({setActionUp()},1000)
         }
         txtSo.text = number.toString()
     }
-    
+
     fun setDown(m: MotionEvent){
 
         z= m.getY().toInt()
@@ -67,10 +117,10 @@ class MainActivity : AppCompatActivity() {
 
     fun setMove(m: MotionEvent){
         if (z<m.getY()){
-            number--
+            number = number!! - 1
             txtSo.text = number.toString()
         }else if (z>m.getY()){
-            number++
+            number = number!! + 1
             txtSo.text = number.toString()
 
         }
@@ -80,39 +130,42 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setActionUp(){
-        thread = Thread(Runnable {
-            if (number > 0) {
 
-                while (number > 0){
-                    number--
+        thread = Thread(Runnable {
+            if (number!! > 0) {
+
+                while (number!! > 0){
+                    number = number!! - 1
                     var msg: Message = Message()
                     msg.what = messageCount
-                    msg.arg1 = number
+                    msg.arg1 = number!!
                     mHandler.sendMessage(msg)
                     Thread.sleep(100)
 
                 }
-
                 number = 0
             }
             else{
 
-                while (number < 0){
-                    number++
+                while (number!! < 0){
+                    number = number!! + 1
                     var msg: Message = Message()
                     msg.what = messageCount
-                    msg.arg1 = number
+                    msg.arg1 = number!!
                     mHandler.sendMessage(msg)
                     Thread.sleep(100)
 
                 }
-
                 number = 0
             }
         })
         thread.start()
 
     }
+    fun delay(function: () -> Unit,delay: Long){
+        mHandler.postDelayed(function,delay)
+    }
+
 
     fun printDisply(){
 
