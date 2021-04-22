14c1c55a06106e86d9d4b714ddb2c4b413d37449
buivanquang111 <58690765+buivanquang111@users.noreply.github.com>
2021-04-22 23:41:30 +0700

    uup
    uup

--- app/src/main/java/com/example/excercise2_monstarlab/MainActivity.kt
+++ app/src/main/java/com/example/excercise2_monstarlab/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.excercise2_monstarlab
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import android.graphics.PorterDuff
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.os.Handler
 import android.os.Message
+import android.util.Log
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
@@ -14,166 +16,113 @@ import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    val messageCount: Int = 1
     var number: Int? = 0
-    var z: Int = 0
+    var y: Int = 0
     var mHandler: Handler = Handler()
-    lateinit var thread: Thread
-    var increase: CountDownTimer? = null
-    var decrease: CountDownTimer? = null
-    var isMoving: Boolean = false
+    lateinit var increases: Runnable
+    lateinit var decreases: Runnable
+    lateinit var backZero: Runnable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        printDisply()
-
-        increase = object : CountDownTimer(Long.MAX_VALUE,100){
-            override fun onFinish() {
-                TODO("Not yet implemented")
-            }
-
-            override fun onTick(millisUntilFinished: Long) {
-                number = number!! + 1
-                txtSo.text = number.toString()
-                if (number == 0){
-                    increase?.cancel()
-
-                }
-            }
-
-        }
-        decrease = object : CountDownTimer(Long.MAX_VALUE,100){
-            override fun onFinish() {
-                TODO("Not yet implemented")
-            }
-
-            override fun onTick(millisUntilFinished: Long) {
-                number = number!! -1
-                txtSo.text = number.toString()
-                if (number == 0){
-                    decrease?.cancel()
-
-                }
-            }
-
-        }
-
-        btnCong.setOnTouchListener {
-            v, event ->
-            if (event.action == MotionEvent.ACTION_DOWN){
-                btnCong.setBackgroundColor(resources.getColor(R.color.green))
-                increase?.start()
-                decrease?.cancel()
-            }
-            if (event.action == MotionEvent.ACTION_UP){
+        backZero = Runnable {
+            if (number!! > 0){
+                number = number!! - 1
+                txtNumber.text = number.toString()
                 btnCong.setBackgroundColor(resources.getColor(R.color.teal_200))
-                increase?.cancel()
-                delay({decrease?.start()},1000)
+                mHandler.postDelayed(backZero,100)
             }
-
-            false
-        }
-        btnTru.setOnTouchListener {
-            v, event ->
-            if (event.action == MotionEvent.ACTION_DOWN){
-                btnTru.setBackgroundColor(resources.getColor(R.color.green))
-                decrease?.start()
-                increase?.cancel()
-            }
-            if (event.action == MotionEvent.ACTION_UP){
+            else if (number!! < 0){
+                number = number!! + 1
+                txtNumber.text = number.toString()
                 btnTru.setBackgroundColor(resources.getColor(R.color.teal_200))
-                decrease?.cancel()
-                delay({increase?.start()},1000)
+                mHandler.postDelayed(backZero,100)
             }
-            false
+            else {
+                mHandler.removeCallbacks(backZero)
+            }
+        }
+       increases = Runnable {
+           mHandler.removeCallbacks(backZero)
+           number = number!! + 1
+           if (btnCong.isPressed){
+               mHandler.postDelayed(increases,100)
+               txtNumber.text = number.toString()
+               randomColor(number!!)
+           }
+           btnCong.setBackgroundColor(resources.getColor(R.color.green))
+           mHandler.postDelayed(backZero, 2000)
+       }
+        decreases = Runnable {
+            mHandler.removeCallbacks(backZero)
+            number = number!! - 1
+            if (btnTru.isPressed){
+                mHandler.postDelayed(decreases,100)
+                txtNumber.text = number.toString()
+                randomColor(number!!)
+            }
+            btnTru.setBackgroundColor(resources.getColor(R.color.green))
+            mHandler.postDelayed(backZero,2000)
         }
 
+
+
+        btnCong.setOnClickListener {
+            mHandler.removeCallbacks(backZero)
+            number = number!! + 1
+            txtNumber.text = number.toString()
+            mHandler.postDelayed(backZero,2000)
+        }
+        btnTru.setOnClickListener {
+            mHandler.removeCallbacks(backZero)
+            number = number!! - 1
+            txtNumber.text = number.toString()
+            mHandler.postDelayed(backZero,2000)
+        }
+        btnCong.setOnLongClickListener {
+            mHandler.post(increases)
+        }
+        btnTru.setOnLongClickListener {
+            mHandler.post(decreases)
+        }
         rvActivity.setOnTouchListener {
             v, event ->  handleTouch(event)
             true
         }
-
-
     }
 
-
-    //xu li vuot
     fun handleTouch(m: MotionEvent){
         var action = m.actionMasked
         when(action){
-            MotionEvent.ACTION_DOWN ->setDown(m)
-            MotionEvent.ACTION_MOVE ->setMove(m)
-            MotionEvent.ACTION_UP -> delay({setActionUp()},1000)
-        }
-        txtSo.text = number.toString()
-    }
-
-    fun setDown(m: MotionEvent){
-
-        z= m.getY().toInt()
-    }
-
-    fun setMove(m: MotionEvent){
-        if (z<m.getY()){
-            number = number!! - 1
-            txtSo.text = number.toString()
-        }else if (z>m.getY()){
-            number = number!! + 1
-            txtSo.text = number.toString()
-
-        }
-        z= m.getY().toInt()
-
-
-    }
-
-    fun setActionUp(){
-
-        thread = Thread(Runnable {
-            if (number!! > 0) {
-
-                while (number!! > 0){
+            MotionEvent.ACTION_DOWN -> {
+                y= m.getY().toInt()
+            }
+            MotionEvent.ACTION_MOVE -> {
+                if (y<m.getY()){
                     number = number!! - 1
-                    var msg: Message = Message()
-                    msg.what = messageCount
-                    msg.arg1 = number!!
-                    mHandler.sendMessage(msg)
-                    Thread.sleep(100)
-
-                }
-                number = 0
-            }
-            else{
-
-                while (number!! < 0){
+                    txtNumber.text = number.toString()
+                    randomColor(number!!)
+                }else if (y>m.getY()){
                     number = number!! + 1
-                    var msg: Message = Message()
-                    msg.what = messageCount
-                    msg.arg1 = number!!
-                    mHandler.sendMessage(msg)
-                    Thread.sleep(100)
-
+                    txtNumber.text = number.toString()
+                    randomColor(number!!)
                 }
-                number = 0
+                y = m.getY().toInt()
             }
-        })
-        thread.start()
-
-    }
-    fun delay(function: () -> Unit,delay: Long){
-        mHandler.postDelayed(function,delay)
-    }
-
-
-    fun printDisply(){
-
-        mHandler = Handler(){
-            txtSo.text = it.arg1.toString()
-            true
+            MotionEvent.ACTION_UP -> {
+                mHandler.postDelayed(backZero,2000)
+            }
         }
+    }
 
+    fun randomColor(number: Int){
+        var random: Random = Random()
+        val color: Int = Color.argb(255,random.nextInt(256),random.nextInt(256),random.nextInt(256))
+        if (number % 100 == 0) {
+            txtNumber.setTextColor(color)
+        }
     }
 
 }
